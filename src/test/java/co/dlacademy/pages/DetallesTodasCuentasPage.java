2ce5bdd3ce373c6b1f16d152bf6ee79d0d9ab1be
dzabala13 <dzabala13@unisalle.edu.co>
2024-07-09 12:50:58 -0500

    refactro validation's accounts

--- src/test/java/co/dlacademy/pages/DetallesTodasCuentasPage.java
+++ src/test/java/co/dlacademy/pages/DetallesTodasCuentasPage.java
@@ -4,17 +4,25 @@ import net.serenitybdd.core.Serenity;
 import net.serenitybdd.core.pages.WebElementFacade;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+import java.util.Map;
+
+import static net.thucydides.model.matchers.BeanMatchers.the;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static net.thucydides.core.pages.components.HtmlTable.rowsFrom;
+import static net.thucydides.core.pages.components.HtmlTable.filterRows;
+import static org.hamcrest.Matchers.*;
+
 
 public class DetallesTodasCuentasPage extends BasePage{
 
-    @FindBy(xpath = "//table[@id='accountTable']//td//a[contains(.,'16119')]")
-    WebElementFacade linkCuentaPropia;
+    @FindBy(xpath = "//table[@id='accountTable']")
+    WebElementFacade table;
 
     public void validarCuentaAbierta(){
         String variableSesionCuentaAperturada = Serenity.sessionVariableCalled("numeroCuenta");
-        assertThat(variableSesionCuentaAperturada, is(equalTo(linkCuentaPropia.getText())));
+        rowsFrom(table);
+        String data = filterRows(table,the("Account",is(variableSesionCuentaAperturada))).get(0).getText().split(" ")[0];
+        assertThat(data, is(equalTo(variableSesionCuentaAperturada)));
     }
 }
