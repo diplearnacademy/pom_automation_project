c82e200807560cb2d9c963617264a53b149c1bf8
valenciv <valenciv@oclc.org>
2024-07-11 17:24:31 -0500

    Agregar aserción

--- src/test/java/co/dlacademy/pages/DetallesTodasCuentasPage.java
+++ src/test/java/co/dlacademy/pages/DetallesTodasCuentasPage.java
@@ -2,7 +2,12 @@ package co.dlacademy.pages;
 
 import net.serenitybdd.core.Serenity;
 import net.serenitybdd.core.pages.WebElementFacade;
+import net.serenitybdd.screenplay.targets.Target;
+import org.junit.Assert;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -10,11 +15,23 @@ import static org.hamcrest.Matchers.is;
 
 public class DetallesTodasCuentasPage extends BasePage{
 
-    @FindBy(xpath = "//table[@id='accountTable']//td//a[contains(.,'16119')]")
-    WebElementFacade linkCuentaPropia;
+    @FindBy(xpath = "//table[@id='accountTable']/tbody/tr/td/a")
+    List<WebElementFacade> linkCuentaPropia;
 
     public void validarCuentaAbierta(){
+
+        boolean flag = false;
+        int rowSize =linkCuentaPropia.size();
         String variableSesionCuentaAperturada = Serenity.sessionVariableCalled("numeroCuenta");
-        assertThat(variableSesionCuentaAperturada, is(equalTo(linkCuentaPropia.getText())));
+        for(int i = 0; i <= rowSize; i++) {
+            if (variableSesionCuentaAperturada.equals(Target.the("Numeros de cuenta").locatedBy("//table[@id='accountTable']/tbody/tr['"+i+"']/td/a")))
+            {
+                flag =true;
+                break;
+            }else{
+                System.out.println("No corresponder al número de cuenta");
+            }
+        }
+        Assert.assertTrue(flag);
     }
 }
