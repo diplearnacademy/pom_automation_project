ee4929bf1c72e662331c9b6e57c3e803b3f38cdc
LinaMar <lmlopezgil@gmail.com>
2024-07-11 12:09:26 -0500

    Realizacion del assert para la solucion del reto1

--- src/test/java/co/dlacademy/pages/DetallesTodasCuentasPage.java
+++ src/test/java/co/dlacademy/pages/DetallesTodasCuentasPage.java
@@ -2,6 +2,7 @@ package co.dlacademy.pages;
 
 import net.serenitybdd.core.Serenity;
 import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.FindBy;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,11 +11,16 @@ import static org.hamcrest.Matchers.is;
 
 public class DetallesTodasCuentasPage extends BasePage{
 
-    @FindBy(xpath = "//table[@id='accountTable']//td//a[contains(.,'16119')]")
+   /* @FindBy(xpath = "//table[@id='accountTable']//td//a[contains(.,'14121')]")*/
+   public WebElementFacade mapeo (String id){
+       String xpath = "//table[@id='accountTable']//td//a[contains(.,'"+id+"')]";
+       return find(By.xpath(xpath));
+   }
     WebElementFacade linkCuentaPropia;
 
     public void validarCuentaAbierta(){
         String variableSesionCuentaAperturada = Serenity.sessionVariableCalled("numeroCuenta");
+        linkCuentaPropia = mapeo(variableSesionCuentaAperturada);
         assertThat(variableSesionCuentaAperturada, is(equalTo(linkCuentaPropia.getText())));
     }
 }
